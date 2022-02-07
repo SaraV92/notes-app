5ddc236917dd38785daf98874d724bc79176104a
saravi92 <sara.viglione92@gmail.com>
2022-02-07 11:39:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Title
+
+#### Type of App, date
+
+## Description
+
+## Technologies
+
+## How to create a blog post
+
+## How to install it
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BorderColorIcon from "@material-ui/icons/BorderColor";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BorderColorIcon />
+        Wall of NOTES
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
